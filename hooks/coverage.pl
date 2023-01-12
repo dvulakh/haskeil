f29a58a52175e58f0f68d4174790466fbbcfada3
David Vulakh <david@vulakh.us>
2023-01-12 10:00:49 -0500

    move coverage reports

--- hooks/coverage.pl
+++ hooks/coverage.pl
@@ -6,5 +6,6 @@ no strict;
 $/ = undef;
 $_ = <STDIN>;
 $_ =~ s|<style.*?</style>||gms;
+$_ =~ s|href="(.*?)"|href="https://htmlpreview.github.io/?https://github.com/dvulakh/haskeil/blob/main/coverage/\1"|mg;
 $_ =~ s|(<td align="right">(\d?\d?\d)\%.*?<td width=100>).*?</td></tr></table></td></tr></table>|\1<img src="https://progress-bar.dev/\2">|gm;
 print "$_";
