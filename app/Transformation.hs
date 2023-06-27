0052cc465cd1ab277603f8c83994a7713ad404ea
avbalsam <avbalsam@gmail.com>
2023-06-27 13:54:10 +0300

    Show link to location on page of transformations

--- app/Transformation.hs
+++ app/Transformation.hs
@@ -9,7 +9,18 @@ data Transformation = Aatat
                     | Ofanim
                     | AkhasBeta
                     | Avgad
-  deriving (Bounded, Enum, Eq, Show)
+  deriving (Bounded, Enum, Eq)
+
+instance Show Transformation where
+  show AkhasBeta = "<a href='#akhasbeta'>Akhas Beta</a>"
+  show AyakBakar = "<a href='#ayakbakar'>Ayak Bakar</a>"
+  show Aatat = "None"
+  show Atbash = "<a href='#atbash'>Atbash</a>"
+  show Albam = "<a href='#albam'>Albam</a>"
+  show Achbi = "<a href='#achbi'>Achbi</a>"
+  show Ofanim = "<a href='ofanim'>Ofanim</a>"
+  show Avgad = "<a href='#avgad'>Avgad</a>"
+  
 
 maxLetter :: Int
 maxLetter = fromEnum (maxBound :: HLetter) + 1
